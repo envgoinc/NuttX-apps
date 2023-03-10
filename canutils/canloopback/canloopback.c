814e080a858e58ec87c0388dbc52c6e3456802aa
Jerry Mailloux <jerrymailloux@gmail.com>
2023-03-10 16:08:33 -0500

    uncommented changes now properly do loopback. Seem to need CONFIG_NET_TCP (not UDP) defined for things to work.

--- canutils/canloopback/canloopback.c
+++ canutils/canloopback/canloopback.c
@@ -46,16 +46,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <time.h>
 #include <poll.h>
 
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <sys/time.h>
+
 
 #include <nuttx/can.h>
 #include <netpacket/can.h>
 
 #include "lib.h"
+
+//static const int canfd_on = 1;
+
 
 static void print_usage_send(char *prg)
 {
@@ -68,8 +75,14 @@ static void print_usage_send(char *prg)
 int main(int argc, char **argv)
 {
 	int s; /* can raw socket */
+	fd_set rdfs;
 	struct sockaddr_can addr;
 	struct ifreq ifr;
+	struct iovec iov;
+	struct msghdr msg;
+	struct can_frame receive_msg;
+	char ctrlmsg[CMSG_SPACE(sizeof(struct timeval) + 3*sizeof(struct timespec) + sizeof(__u32))];
+	int ret;
 
 	/* check command line options */
 	if (argc != 2) {
@@ -78,7 +91,8 @@ int main(int argc, char **argv)
 	}
 
 	/* open socket */
-	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+	s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+	if (s < 0) {
 		perror("socket");
 		return 1;
 	}
@@ -90,20 +104,88 @@ int main(int argc, char **argv)
 		perror("if_nametoindex");
 		return 1;
 	}
+	printf("interface: %s\n", ifr.ifr_name);
 
 	memset(&addr, 0, sizeof(addr));
 	addr.can_family = AF_CAN;
 	addr.can_ifindex = ifr.ifr_ifindex;
 
 	/* disable default receive filter on this RAW socket */
-	setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+	// This makes recvmsg block indefinitely!!!
+	//setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+
+	/* try to switch the socket into CAN FD mode */
+	//setsockopt(s, SOL_CAN_RAW, CAN_RAW_FD_FRAMES, &canfd_on, sizeof(canfd_on));
 
 	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		perror("bind");
 		return 1;
 	}
 
+	iov.iov_base = &receive_msg;
+	msg.msg_name = &addr;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = &ctrlmsg;
+
+
 	while(1) {
+#if 0
+		FD_ZERO(&rdfs);
+		FD_SET(s, &rdfs);
+
+		printf("calling select\n");
+
+		if ((ret = select(s+1, &rdfs, NULL, NULL, NULL)) <= 0) {
+			perror("select");
+			continue;
+		}
+		printf("select returned!\n");
+
+		if (FD_ISSET(s, &rdfs)) {
+			iov.iov_len = sizeof(receive_msg);
+			msg.msg_namelen = sizeof(addr);
+			msg.msg_controllen = sizeof(ctrlmsg);
+			msg.msg_flags = 0;
+			printf("calling recvmsg\n");
+
+			const ssize_t nbytes = recvmsg(s, &msg, 0);
+
+			if (nbytes < 0 || (size_t)nbytes > sizeof(receive_msg)) {
+				// error - todo - could this happen if we read in the middle of a packet receive?
+				printf("error on receive. Bytes read: %d, error %d\n", nbytes, errno);
+			} else {
+				printf("trying to send socket back\n");
+				// send what was received
+				//fprintf(stderr, "send 0x%lx\n", receive_msg.can_id);
+				if (send(s, &receive_msg, sizeof(receive_msg), 0) != sizeof(receive_msg)) {
+					perror("send");
+					return 1;
+				}
+			}
+		}
+#endif
+
+		//printf("trying to read\n");
+
+		// Try to read.
+		struct can_frame receive_msg;
+		const ssize_t nbytes = read(s, &receive_msg, sizeof(receive_msg));
+		//printf("read: 0x%lx, \n", receive_msg.can_id);
+
+		if (nbytes < 0 || (size_t)nbytes > sizeof(receive_msg)) {
+			// error - todo - could this happen if we read in the middle of a packet receive?
+			printf("error on receive. Bytes read: %d\n", nbytes);
+		} else {
+			// send what was received
+			//printf("send 0x%lx\n", receive_msg.can_id);
+			if (write(s, &receive_msg, sizeof(receive_msg)) != sizeof(receive_msg)) {
+				perror("write");
+				return 1;
+			}
+		}
+
+#if 0
 		// File desriptor for CAN.
 		struct pollfd fds;
 		int result;
@@ -112,33 +194,36 @@ int main(int argc, char **argv)
 		fds.fd = s;
 		fds.events = POLLIN;
 
+		printf("calling poll\n");
+
 		// Any received CAN messages will cause the poll statement to unblock and run
 		// This way CAN read runs with minimal latency.
 		// Note that multiple messages may be received in a short time, so this will try to read any availible in a loop
 		result = poll(&fds, 1, -1);
 		//if (result < 0) {
-			fprintf(stderr, "poll returned %d.  ", result);
+			printf("poll returned %d\n", result);
 		//}
 
 		// Only execute this part if can0 is changed.
 		if (fds.revents & POLLIN) {
 			// Try to read.
 			struct can_frame receive_msg;
-			const ssize_t nbytes = read(fds.fd, &receive_msg, sizeof(receive_msg));
-			fprintf(stderr, "read: 0x%lx, ", receive_msg.can_id);
+			const ssize_t nbytes = read(s, &receive_msg, sizeof(receive_msg));
+			printf("read: 0x%lx, ", receive_msg.can_id);
 
 			if (nbytes < 0 || (size_t)nbytes > sizeof(receive_msg)) {
 				// error - todo - could this happen if we read in the middle of a packet receive?
-				fprintf(stderr, "error on receive. Bytes read: %d\n", nbytes);
+				printf("error on receive. Bytes read: %d\n", nbytes);
 			} else {
 				// send what was received
-				fprintf(stderr, "send 0x%lx\n", receive_msg.can_id);
-				if (write(fds.fd, &receive_msg, sizeof(receive_msg)) != sizeof(receive_msg)) {
+				printf("send 0x%lx\n", receive_msg.can_id);
+				if (write(s, &receive_msg, sizeof(receive_msg)) != sizeof(receive_msg)) {
 					perror("write");
 					return 1;
 				}
 			}
 		}
+#endif
 	}
 
 	close(s);
